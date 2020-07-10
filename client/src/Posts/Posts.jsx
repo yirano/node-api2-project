77a68bd846c203d0bf83dffd8c85c3f4af6320e7
yirano <yirano@protonmail.ch>
2020-07-10 13:41:15 -0400

    User can delete a post

--- client/src/Posts/Posts.jsx
+++ client/src/Posts/Posts.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import Axios from 'axios'
 
 const Posts = () => {
   const [posts, setPosts] = useState([])
@@ -10,7 +11,18 @@ const Posts = () => {
         setPosts(res.data.data)
       })
   }, [])
-  console.log(posts)
+
+  const handleDelete = (id, e) => {
+
+    Axios.delete(`http://localhost:8000/api/posts/${id}`)
+      .then(res => {
+        console.log(res)
+        window.location.reload()
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
   return (
     <div>
       {posts.map(post => {
@@ -19,6 +31,8 @@ const Posts = () => {
             <h3>{post.title}</h3>
             <p>{post.created_at}</p>
             <p>{post.contents}</p>
+            <button onClick={(e) => handleDelete(post.id, e)}>Delete Post</button>
+
           </div>
         )
       })}
