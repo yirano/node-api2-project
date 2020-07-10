bdba263186440d3867e6efb4ddeb4479743d8303
yirano <yirano@protonmail.ch>
2020-07-10 00:58:15 -0400

    User can post by making a POST request to /api/posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -4,6 +4,25 @@ const db = require('../data/db')
 
 const router = express.Router()
 
+router.post('/', async (req, res) => {
+  const blogPost = req.body
+  try {
+    if (blogPost.title && blogPost.contents) {
+      const posted = await db.insert(blogPost)
+      if (posted) {
+        res.status(201).json({ data: posted })
+      } else {
+        res.status(404).json({ errorMessage: 'The post could not be posted' })
+      }
+    } else {
+      res.status(400).json({ errorMessage: 'The server could not understand the request due to invalid syntax.' })
+    }
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ errorMessage: 'Something went wrong' })
+  }
+})
+
 router.get('/', async (req, res) => {
   const posts = await db.find()
 
@@ -18,5 +37,7 @@ router.get('/', async (req, res) => {
     res.status(500).json({ errorMessage: `Something went wrong: ${err}` })
   }
 })
+
+
 
 module.exports = router
